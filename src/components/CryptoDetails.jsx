fff4fd725ab215131b9c081914ce48ed9cd0e275
ujjwal <ujjwalpratik2001@gmail.com>
2022-04-12 09:38:54 +0530

    initial commit

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CryptoDetails = () => {
+  return (
+    <div>
+        CryptoDetails
+    </div>
+  )
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cryptocurrencies = () => {
+  return (
+    <div>
+        Cryptocurrencies
+    </div>
+  )
+}
+
+export default Cryptocurrencies
